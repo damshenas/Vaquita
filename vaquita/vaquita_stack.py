c32fc8c79aedf119c119bb20fb670e83e485ae17
mohsen-aws <mohsend@f8ffc245ac9a.ant.amazon.com>
2020-08-06 15:28:17 +0200

    added dynamodb to stack

--- vaquita/vaquita_stack.py
+++ vaquita/vaquita_stack.py
@@ -5,8 +5,9 @@ from aws_cdk import (
     aws_s3 as _s3,
     aws_cognito as _cognito,
     aws_sqs as _sqs,
+    aws_dynamodb as _dydb,
     # aws_cloudfront as _cloudFront,
-    aws_elasticsearch as _esearch,
+    # aws_elasticsearch as _esearch,
     aws_apigateway as _apigw,
     aws_iam as _iam,
     # aws_ec2 as _ec2,
@@ -45,31 +46,47 @@ class VaquitaStack(core.Stack):
         apiGatewayGetSignedUrlResource = apiGatewayResource.add_resource('signedUrl')
         apiGatewayImageSearchResource = apiGatewayResource.add_resource('search')
 
-        ### elastic search
-        esearchDocument = _iam.PolicyDocument()
-        esearchStatement = _iam.PolicyStatement(
-            effect=_iam.Effect.ALLOW, 
-            actions=["es:*",]
+        ### create dynamo table
+        dynamodb_table = _dydb.Table(
+            self, "VAQUITA_TABLE",
+            partition_key=_dydb.Attribute(
+                name="id",
+                type=_dydb.AttributeType.STRING
+            ),
+            sort_key=_dydb.Attribute(
+                name="label",
+                type=_dydb.AttributeType.STRING
+            ),
+            billing_mode=_dydb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=core.RemovalPolicy.DESTROY # NOT recommended for production code
         )
 
-        esearchStatement.add_aws_account_principal(core.Aws.ACCOUNT_ID)
-        esearchStatement.add_resources("arn:aws:es:{}:{}:domain/VAQUITA_ELASTIC_SEARCH/*".format(core.Aws.REGION, core.Aws.ACCOUNT_ID))
-        esearchDocument.add_statements(esearchStatement)
 
-        elasticSearch = _esearch.CfnDomain(self, "VAQUITA_ELASTIC_SEARCH",
-            elasticsearch_version="2.3",
-            access_policies=esearchDocument,
-            elasticsearch_cluster_config={
-                "InstanceCount": "1",
-                "InstanceType": "t2.micro.elasticsearch",
-                "DedicatedMasterEnabled": False,
-                "zoneAwarenessEnabled": False,
-                },
-            ebs_options={
-                "ebsEnabled": True, 
-                "volumeSize": 10
-                }
-            )
+        ### elastic search
+        # esearchDocument = _iam.PolicyDocument()
+        # esearchStatement = _iam.PolicyStatement(
+        #     effect=_iam.Effect.ALLOW, 
+        #     actions=["es:*",]
+        # )
+
+        # esearchStatement.add_aws_account_principal(core.Aws.ACCOUNT_ID)
+        # esearchStatement.add_resources("arn:aws:es:{}:{}:domain/VAQUITA_ELASTIC_SEARCH/*".format(core.Aws.REGION, core.Aws.ACCOUNT_ID))
+        # esearchDocument.add_statements(esearchStatement)
+
+        # elasticSearch = _esearch.CfnDomain(self, "VAQUITA_ELASTIC_SEARCH",
+        #     elasticsearch_version="2.3",
+        #     access_policies=esearchDocument,
+        #     elasticsearch_cluster_config={
+        #         "InstanceCount": "1",
+        #         "InstanceType": "t2.micro.elasticsearch",
+        #         "DedicatedMasterEnabled": False,
+        #         "zoneAwarenessEnabled": False,
+        #         },
+        #     ebs_options={
+        #         "ebsEnabled": True, 
+        #         "volumeSize": 10
+        #         }
+        #     )
 
         ### landing page function
         getLandingPageFunction = _lambda.Function(self, "VAQUITA_GET_LANDING_PAGE",
@@ -181,7 +198,7 @@ class VaquitaStack(core.Stack):
             environment={
                 "VAQUITA_IMAGES_BUCKET": imagesS3Bucket.bucket_name,
                 "REGION": core.Aws.REGION,
-                "ES_HOST": elasticSearch.attr_domain_endpoint,
+                "TABLE_NAME": dynamodb_table.table_name,
                 "ES_INDEX": "images"
                 },
             handler="main.handler",
@@ -196,15 +213,9 @@ class VaquitaStack(core.Stack):
             resources=["*"]                    
         )
 
-        lambda_elasticsearch_access_analyzer = _iam.PolicyStatement(
-            effect=_iam.Effect.ALLOW, 
-            actions=["es:ESHttp*"],
-            resources=["*"] #tbc [elasticSearch.attr_arn]              
-        ) 
-
         imageAnalyzerFunction.add_to_role_policy(lambda_rekognition_access)
-        imageAnalyzerFunction.add_to_role_policy(lambda_elasticsearch_access_analyzer)
         imagesS3Bucket.grant_read(imageAnalyzerFunction, "processed/*")
+        dynamodb_table.grant_write_data(imageAnalyzerFunction)
         
         ### image search function
         imageSearchFunction = _lambda.Function(self, "VAQUITA_IMAGE_SEARCH",
@@ -214,7 +225,7 @@ class VaquitaStack(core.Stack):
             environment={
                 "VAQUITA_IMAGES_BUCKET": imagesS3Bucket.bucket_name,
                 "REGION": core.Aws.REGION,
-                "ES_HOST": elasticSearch.attr_domain_endpoint,
+                "TABLE_NAME": dynamodb_table.table_name,
                 "ES_INDEX": "images"
                 },
             handler="main.handler",
@@ -248,13 +259,13 @@ class VaquitaStack(core.Stack):
             ).node.find_child('Resource').add_property_override('AuthorizerId', apiGatewayImageSearchAuthorizer.ref)
 
 
-        lambda_elasticsearch_access_search = _iam.PolicyStatement(
+        lambda_access_search = _iam.PolicyStatement(
             effect=_iam.Effect.ALLOW, 
-            actions=["es:ESHttp*", "translate:TranslateText"],
+            actions=["dynamodb:Query", "translate:TranslateText"],
             resources=["*"] #tbc [elasticSearch.attr_arn]              
         ) 
 
-        imageSearchFunction.add_to_role_policy(lambda_elasticsearch_access_search)
+        imageSearchFunction.add_to_role_policy(lambda_access_search)
 
         ### API gateway finalizing
         self.add_cors_options(apiGatewayGetSignedUrlResource)
