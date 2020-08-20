9201e8c2397a493b86e4a31763dfa2eaac743fea
mohsen-aws <mohsend@f8ffc245ac9a.ant.amazon.com>
2020-08-20 12:30:29 +0200

    removed dynamodb and added Aurora MySQL

--- src/database/main.py
+++ src/database/main.py
@@ -1,69 +1,39 @@
-import boto3
-import botocore.config
 import os
 import logging
 import sys
 import json
 
-aws_config = botocore.config.Config(
-    region_name = os.getenv('REGION'),
-    signature_version = 'v4',
-    retries = {
-        'max_attempts': 5,
-        'mode': 'standard'
-    }
-)
-
 script_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, "{}/assets".format(script_path))
+
+import pymysql as mysql
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-dynamodb_resource = boto3.resource('dynamodb', config=aws_config)
-dynamodb_table = dynamodb_resource.Table(os.getenv('TABLE_NAME'))
+try:
+    conn = mysql.connect(os.environ['HOSTNAME'], user=os.environ['USERNAME'], passwd=os.environ['PASSWORD'], db=os.environ['DB_NAME'], connect_timeout=5)
+except mysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    logger.error(e)
+    sys.exit()
 
-rekognition_client = boto3.client('rekognition', config=aws_config)
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
 def handler(event, context):
+    item_count = 0
 
-    for record in event['Records']:
-        # receiptHandle = record['receiptHandle']
-        body = record['body']
-        message = json.loads(body)
+    with conn.cursor() as cur:
+        cur.execute("create table Employee ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))")
+        cur.execute('insert into Employee (EmpID, Name) values(1, "Joe")')
+        cur.execute('insert into Employee (EmpID, Name) values(2, "Bob")')
+        cur.execute('insert into Employee (EmpID, Name) values(3, "Mary")')
+        conn.commit()
+        cur.execute("select * from Employee")
+        for row in cur:
+            item_count += 1
+            logger.info(row)
+            #print(row)
+    conn.commit()
 
-        bucket = os.environ['VAQUITA_IMAGES_BUCKET']
-        key = message['image']
-        # original_key = message['original_key']
-        # original_last_modified = message['original_last_modified']
-        # etag = message['etag']
-
-        logger.info('Processing {}.'.format(key))
-
-        detected_labels = rekognition_client.detect_labels(
-            Image={'S3Object': {'Bucket': bucket, 'Name': key}},
-            MaxLabels=20,
-            MinConfidence=85)
-            
-        detected_unsafe_contents = rekognition_client.detect_moderation_labels(
-            Image={'S3Object': {'Bucket': bucket, 'Name': key}})
-               
-        object_labels = []
-
-        for l in detected_labels['Labels']:
-            object_labels.append(l['Name']) # add objects in image
-
-        for l in detected_unsafe_contents['ModerationLabels']:
-            object_labels.append(l['Name'])
-            object_labels.append("offensive") #label image as offensive
-
-        image_id = key.split("/")[-1]
-
-        with dynamodb_table.batch_writer() as batch:
-            for label in object_labels:
-                dynamodb_record = {'id':image_id, 'label': label.lower()}
-                batch.put_item(Item=dynamodb_record)
-
-        logger.info("Image is indexed: {}: {}".format(image_id, object_labels))
-
-    return True
+    return "Added %d items from RDS MySQL table" %(item_count)
